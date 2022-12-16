4da534c18caf42767be3387454045f1891b85cb2
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-12-16 12:26:19 -0700

    Update to use keyword this instead of targeting counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,11 @@ $(document).ready(function() {
   let max = 140;
   $("#tweet-text").on("input",function() {
     let length = max - $(this).val().length
-    $(".counter").text(length)
-
+    $(this).parent().find('output').text(length)
     color = length < 0 ? 'red' : '#545149';
-    $('.counter').css({
+    $(this).parent().find('output').css({
     'color': color
-  });
-
+    });
   });
 });
+
