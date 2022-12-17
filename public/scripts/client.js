eb644bbef163733b3b9c96bd1422cf9939e6261e
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-12-17 14:13:54 -0700

    Add get request to fetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,37 +4,19 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+ $(document).ready(function() {
+  const loadTweets = function(){
+    $.ajax('http://localhost:8080/tweets', { method: 'GET' }) 
+      .then(function (tweets) {
+        renderTweets(tweets);
+      });
+    }
+    loadTweets();
 
 
 const renderTweets = function(tweets) {
   // loops through tweets
-  data.forEach(tweet => {
+  tweets.forEach(tweet => {
     // calls createTweetElement for each tweet
     const tweetHtml = createTweetElement(tweet);
     $('.tweets-container').append(tweetHtml);
@@ -43,7 +25,7 @@ const renderTweets = function(tweets) {
 
 //Responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
 const createTweetElement = function(tweet) {
-const date = new Date(tweet.created_at)  
+const date = timeago.format(tweet.created_at)  
   let $tweet = $(`<article class="new-tweet">
     <header class="tweet-header"> 
       <i class="fa-solid fa-user-tie"></i>
@@ -51,7 +33,7 @@ const date = new Date(tweet.created_at)
      </header>
       <div class="usertag">${tweet.user.handle}</div>
       <div class="old-tweet">${tweet.content.text}</div>
-      <div class="date">${date.getDay()} days ago</div>
+      <div class="date">${date}</div>
       <div class="icons">
         <i id="flag" class="fa-solid fa-flag"></i>
         <i id="retweet" class="fa-solid fa-retweet"></i>
@@ -60,9 +42,6 @@ const date = new Date(tweet.created_at)
   </article>`);
   return $tweet;
  }
-
-renderTweets(data);
-
 
 $('#tweet').on('submit', function(event){
   event.preventDefault()
@@ -76,7 +55,7 @@ $('#tweet').on('submit', function(event){
     })
 });
 
-
+});
 
 
 
