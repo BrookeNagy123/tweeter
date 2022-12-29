7ca44996bbaa31e4d20c444693f48c468ae0f08a
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-12-29 16:10:02 -0700

    Add comments to describe each function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,23 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+//Responsible for loading existing tweets.
 $(document).ready(function() {
   const loadTweets = function() {
     $.ajax('http://localhost:8080/tweets', { method: 'GET' })
       .then(function(tweets) {
-        renderTweets(tweets);
+        renderTweets(tweets); 
       });
   };
   loadTweets();
 
-
+  //Responsible for showing and adding to the tweets container. 
   const renderTweets = function(tweets) {
     $('.tweets-container').empty();
     tweets.forEach(tweet => {
-    // calls createTweetElement for each tweet
-      const tweetHtml = createTweetElement(tweet);
+      const tweetHtml = createTweetElement(tweet); //Calls createTweetElement function for each tweet
       $('.tweets-container').prepend(tweetHtml);
     });
   };
 
+  //Responsible for e-encoding text so that unsafe characters are converted into a safe "encoded" representation.
   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
@@ -31,9 +26,9 @@ $(document).ready(function() {
 
   //Responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
   const createTweetElement = function(tweet) {
-    const date = timeago.format(tweet.created_at);
-    const safeHTML = `${escape(tweet.content.text)}`;
-    let $tweet = $(`<article class="new-tweet">
+    const date = timeago.format(tweet.created_at); //Use timeago to format date
+    const safeHTML = `${escape(tweet.content.text)}`; //Use the escape function for safe characters
+    let $tweet = $(`<article class="tweets">
     <header class="tweet-header"> 
       <i class="fa-solid fa-user-tie"></i>
       <div class="user">${tweet.user.name}</div>
@@ -50,26 +45,29 @@ $(document).ready(function() {
     return $tweet;
   };
 
+  //Responsible for form submission of a new tweet and error handling if input is invalid. 
   $('#tweet').on('submit', function(event) {
     $(".error").slideUp("medium");
     $(".errorLength").slideUp("medium");
-    event.preventDefault();
+    event.preventDefault();  //Prevent the page from reloading 
     const textarea = document.querySelector("#tweet-text");
     const textareaValue = textarea.value;
-    if (textareaValue === undefined || textareaValue === null || textareaValue === "") {
+    if (textareaValue === undefined || textareaValue === null || textareaValue === "") { //Error handling 
       $(".error").slideDown("slow");
       return;
     } else if (textareaValue.length > 140) {
       $(".errorLength").slideDown("slow");
       return;
     }
-    $.ajax({
+    $.ajax({ //POST request
       url: 'http://localhost:8080/tweets/',
       type: 'POST',
       data: $('#tweet').serialize()
     })
       .then(response => {
         loadTweets();
+        this.reset(); //reset input text box 
+        $("#tweet").find('.counter').text('140'); //reset counter to 140 
       });
   });
 });
