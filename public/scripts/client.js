9bbc6fba14ffba21ab5ac836e08d53e7353a8a8a
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-12-29 13:17:31 -0700

    Eslint the file and clean up syntax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,36 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- $(document).ready(function() {
-  const loadTweets = function(){
-    $.ajax('http://localhost:8080/tweets', { method: 'GET' }) 
-      .then(function (tweets) {
+$(document).ready(function() {
+  const loadTweets = function() {
+    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+      .then(function(tweets) {
         renderTweets(tweets);
       });
-    }
-    loadTweets();
+  };
+  loadTweets();
 
 
-const renderTweets = function(tweets) {
-  $('.tweets-container').empty();
-  tweets.forEach(tweet => {
+  const renderTweets = function(tweets) {
+    $('.tweets-container').empty();
+    tweets.forEach(tweet => {
     // calls createTweetElement for each tweet
-    const tweetHtml = createTweetElement(tweet);
-    $('.tweets-container').prepend(tweetHtml);
-  })
-}
+      const tweetHtml = createTweetElement(tweet);
+      $('.tweets-container').prepend(tweetHtml);
+    });
+  };
 
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-//Responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
-const createTweetElement = function(tweet) {
-const date = timeago.format(tweet.created_at)  
-const safeHTML = `${escape(tweet.content.text)}`;
-  let $tweet = $(`<article class="new-tweet">
+  //Responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+  const createTweetElement = function(tweet) {
+    const date = timeago.format(tweet.created_at);
+    const safeHTML = `${escape(tweet.content.text)}`;
+    let $tweet = $(`<article class="new-tweet">
     <header class="tweet-header"> 
       <i class="fa-solid fa-user-tie"></i>
       <div class="user">${tweet.user.name}</div>
@@ -47,32 +47,31 @@ const safeHTML = `${escape(tweet.content.text)}`;
         <i id="heart" class="fa-solid fa-heart"></i>
       </div>
   </article>`);
-  return $tweet;
- }
+    return $tweet;
+  };
 
-$('#tweet').on('submit', function(event){
-  $( ".error" ).slideUp( "medium");
-  $( ".errorLength" ).slideUp( "medium");
-  event.preventDefault()
-  const textarea = document.querySelector("#tweet-text");
-  const textareaValue = textarea.value;
-  if(textareaValue === undefined || textareaValue === null || textareaValue === ""){
-    $( ".error" ).slideDown("slow");
-    return;
-  } else if (textareaValue.length > 140){
-    $( ".errorLength" ).slideDown("slow");
-    return;
-  }
-  $.ajax({
-    url: 'http://localhost:8080/tweets/',
-    type: 'POST',
-    data: $('#tweet').serialize()
+  $('#tweet').on('submit', function(event) {
+    $(".error").slideUp("medium");
+    $(".errorLength").slideUp("medium");
+    event.preventDefault();
+    const textarea = document.querySelector("#tweet-text");
+    const textareaValue = textarea.value;
+    if (textareaValue === undefined || textareaValue === null || textareaValue === "") {
+      $(".error").slideDown("slow");
+      return;
+    } else if (textareaValue.length > 140) {
+      $(".errorLength").slideDown("slow");
+      return;
+    }
+    $.ajax({
+      url: 'http://localhost:8080/tweets/',
+      type: 'POST',
+      data: $('#tweet').serialize()
     })
-    .then(response => {
-      loadTweets();
-    })
-});
-
+      .then(response => {
+        loadTweets();
+      });
+  });
 });
 
 
