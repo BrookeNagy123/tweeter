b3ccd19ed29b12a42232909a0ae6155c57237764
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-12-29 13:17:52 -0700

    Eslint the file and clean up syntax

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   let max = 140;
   $("#tweet-text").on("input",function() {
-    let length = max - $(this).val().length
-    $(this).parent().find('output').text(length)
+    let length = max - $(this).val().length;
+    $(this).parent().find('output').text(length);
     color = length < 0 ? 'red' : '#545149';
     $(this).parent().find('output').css({
-    'color': color
+      'color': color
     });
   });
 });
