8edd395d49342b7255745e08f2ceee5e4a008bff
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-01-02 15:06:05 -0700

    Add logic for scroll to top button

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,4 @@
 // JQuery used to locate counter and on input will reduce counter from 140. After the user goes past a count of 140 the counter will display red font. 
-
 $(document).ready(function() {
   let max = 140;
   $("#tweet-text").on("input",function() {
@@ -10,5 +9,25 @@ $(document).ready(function() {
       'color': color
     });
   });
+
+ //Button to scroll to top. 
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 200)
+    { $("#topBtn").fadeIn();
+    $("nav").slideUp("medium");
+    } else
+    { $("#topBtn").fadeOut();
+     $("nav").slideDown("medium");
+    }
+  });
+  
+  //When scroll to top button is clicked it will open the tweet form and focus on the tweet text area.
+   $("#topBtn").click(function(){
+      $('html, body').animate({scrollTop: 0},800);
+      $("#tweet").slideDown("medium"); 
+      $("#tweet-text").focus();
+      return false;           
+  });
+
 });
 
